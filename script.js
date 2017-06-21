1f5c14efc074a302dc29f674443e5d39be309cbf
ben-scully <ben@moregolf.co.nz>
2017-06-21 20:13:17 +1200

    fixed regex

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 function isEmailValid() {
-  var regex = '/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/';
+  var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   var email = document.getElementById("email").value;
-  return email.match(regex)
+  return email.match(regex);
 }
 
 function validateForm() {
